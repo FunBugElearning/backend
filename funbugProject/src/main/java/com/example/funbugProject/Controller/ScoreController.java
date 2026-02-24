c650d509306a52f6644339713bf0cb29cbffbabf
Khoa Mai <dangkhoa.dev05@gmail.com>
2026-02-23 22:59:24 -0800

    feat/ adding docker-compose running backend and database

--- funbugProject/src/main/java/com/example/funbugProject/Controller/ScoreController.java
+++ funbugProject/src/main/java/com/example/funbugProject/Controller/ScoreController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/scores")
+@RequestMapping("api/v1/scores")
 public class ScoreController {
 
     private final ScoreService scoreService;
--- funbugProject/src/main/java/com/example/funbugProject/Entity/UserProfile.java
+++ funbugProject/src/main/java/com/example/funbugProject/Entity/UserProfile.java
@@ -2,4 +2,7 @@ package com.example.funbugProject.Entity;
 
 //User Profile Entity
 public class UserProfile {
+    private int id;
+    private User user;
+    private String bio;
 }
